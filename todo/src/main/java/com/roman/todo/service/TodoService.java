afeff2dd68289e7ff66b6f979d5aff8964a18cfe
romanislamali <romanislam897@gmail.com>
2025-01-09 12:04:31 +0600

    initial commit

---
+++ todo/src/main/java/com/roman/todo/service/TodoService.java
@@ -0,0 +1,11 @@
+package com.roman.todo.service;
+
+import com.roman.todo.model.Todo;
+
+import java.util.List;
+
+public interface TodoService {
+    Todo saveTodo(Todo todo);
+    List<Todo> findAllTodo();
+    void deleteTodo();
+}
