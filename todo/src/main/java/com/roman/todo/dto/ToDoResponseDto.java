b48350fb357635237cb7955466216c1a06173e84
romanislamali <romanislam897@gmail.com>
2025-01-14 17:44:08 +0600

    update

--- todo/src/main/java/com/roman/todo/dto/ToDoResponseDto.java
+++ todo/src/main/java/com/roman/todo/dto/ToDoResponseDto.java
@@ -1,8 +1,5 @@
 package com.roman.todo.dto;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- todo/src/main/java/com/roman/todo/service/TodoService.java
+++ todo/src/main/java/com/roman/todo/service/TodoService.java
@@ -2,7 +2,6 @@ package com.roman.todo.service;
 
 import com.roman.todo.dto.ToDoResponseDto;
 import com.roman.todo.dto.TodoRequestDto;
-import com.roman.todo.model.Todo;
 
 import java.util.List;
 
